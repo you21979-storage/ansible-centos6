742f9385dd6fcd20426bcb44568f42f3a3fc84be
you21979 <you2197901@gmail.com>
2013-09-24 16:00:07 +0900

    mod

--- ansible-centos-install.sh
+++ ansible-centos-install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rpm -ivh http://ftp.riken.jp/Linux/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
 yum install -y libyaml PyYAML ansible
+yum install -y git
+git clone git@github.com:you21979/ansible-centos6.git
